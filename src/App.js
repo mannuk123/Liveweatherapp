24720d2d33c6fd9f95a2ff188a9f3efc19dfaf8f
mannuk123 <107272753+mannuk123@users.noreply.github.com>
2022-09-08 21:55:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css"
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import './App.css';
 
 function App() {
